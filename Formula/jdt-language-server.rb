e9b8681cacd131f2f4cd10f275680eb39e0f1eab
Daniel Salmeron Amselem <daniel.amselem@gmail.com>
2018-01-30 10:20:40 +0200

    Add jdt-language-server formula

---
+++ Formula/jdt-language-server.rb
@@ -0,0 +1,26 @@
+class JdtLanguageServer < Formula
+  desc "A Java language specific implementation of the Language Server Protocol"
+  homepage "https://github.com/eclipse/eclipse.jdt.ls"
+
+  url "http://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.12.0-201801300509.tar.gz"
+  sha256 "c38b503fa0c3a5ad8823a512e6a41ba07841e411d5ca1ee41c6adb031dcd3525"
+  version "0.12.0-201801300509"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    rm_rf "config_linux"
+    rm_rf "config_win"
+    libexec.install ["config_mac", "features", "plugins"]
+
+    (bin/"jdt-ls").write <<-EOS.undent
+      #!/bin/bash
+      JDT_LS_HOME="#{libexec}"
+      JDT_LS_LAUNCHER=$(find $JDT_LS_HOME -name "org.eclipse.equinox.launcher_*.jar")
+      JDT_LS_HEAP_SIZE=${JDT_LS_HEAP_SIZE:=-Xmx1G}
+      java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify $JDT_LS_HEAP_SIZE -jar "$JDT_LS_LAUNCHER" -configuration "$JDT_LS_HOME/config_mac" --add-modules=ALL-SYSTEM --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED -data "$1"
+    EOS
+  end
+end
